989bdcc904fb5e856d2ce8d8935304a64bce759f
David <ds90289@yahoo.com>
2020-11-11 01:20:08 +0800

    the enter key will submit messages

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,22 @@ $(document).ready(function() {
 
 	var socket = io();
 
+	var input = document.getElementById("app-conversation-input")
 
 	var hasUserName = false;
 	var username = "";
 	var connected = false;
+
+	input.addEventListener("keyup", function(event) {
+		// Number 13 is the "Enter" key on the keyboard
+		if (event.keyCode === 13) {
+		  // Cancel the default action, if needed
+		  event.preventDefault();
+		  // Trigger the button element with a click
+		  $("#app-conversation-input-submit").click();
+		}
+		
+	});
 	
 	$("#app-conversation-input-submit").click( function() {
 
